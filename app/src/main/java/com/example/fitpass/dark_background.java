7562342d980fc74cb6d8350b046d81ab61c68ca8
ASUS <nbeladiya393@gmail.com>
2023-04-02 22:18:01 +0530

    commit 1

---
+++ app/src/main/java/com/example/fitpass/dark_background.java
@@ -0,0 +1,14 @@
+package com.example.fitpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dark_background extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dark_background);
+    }
+}
